87d38ce4bd1a70d7a43e374590f3414bd389aea5
zhangzhao <zhangz@133.cn>
2019-07-16 14:21:39 +0800

    fix: fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component {
+    constructor (props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
+
+    handleChange (e) {{
+        this.setState({temperature: e.target.value});
+    }}
+
+    render () {
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input
+                    type="text"
+                    value={temperature}
+                    onChange={this.handleChange}
+                />
+            </fieldset>
+        )
+    }
 }
 
-export default App;
+function BoilingVerdict (props) {
+    if (props.celsius >= 100) {
+        return <p>The Water would boil.</p>;
+    }
+
+    return <p>The Water would not boil.</p>;
+}
+
+function toCelsius (fahrenheit) {
+    return (fahrenheit - 32) * 5 /9;
+}
+
+function tpFahrenheit (celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+export default Calculator;
